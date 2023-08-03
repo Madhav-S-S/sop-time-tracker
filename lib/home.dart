a727c65c0d82163e644347cb2eb2da38512123a9
Madhav S S <madhavgs2002@gmail.com>
2023-08-03 12:48:45 +0530

    debug banner removed from home page

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SOP Tracker', // Change app name to "SOP Tracker"
       theme: ThemeData(
         primarySwatch: createMaterialColor(Color(0xFF2BD2B4)), // Set primary color
