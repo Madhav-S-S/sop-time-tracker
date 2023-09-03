8658c4c98e35905fe770170747cd13094c6245c3
Midhun <midhunworks29@gmail.com>
2023-09-04 00:53:39 +0530

    Recording updated

--- lib/createProcess/recordingScreen/recording.dart
+++ lib/createProcess/recordingScreen/recording.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:async';
 
 class RecordingPage extends StatefulWidget {
   final List<Map<String, String>> variableList;
@@ -12,15 +13,24 @@ class RecordingPage extends StatefulWidget {
 
 class _RecordingPageState extends State<RecordingPage> {
   bool _isPlaying = false;
-  int _selectedIndex = -1; // Initialize selected index to -1 (no selection)
-  int _prevSelectedIndex = -1; // Keep track of previously selected index
+  int _selectedIndex = -1;
+  int _prevSelectedIndex = -1;
   int _actionCount = 0;
-  String _startTime = '';
-  Map<int, String> _lastActionMap = {}; // Map to store last actions
-
-  // A stream that emits a value every second
+  DateTime? _startTime;
+  Map<int, String> _lastActionMap = {};
   Stream<DateTime> _timeStream =
-  Stream.periodic(const Duration(seconds: 1), (_) => DateTime.now());
+      Stream.periodic(const Duration(seconds: 1), (_) => DateTime.now());
+
+  // Timer to update action time every second
+  Timer? _actionTimeTimer;
+  // Flag to track if action time has started
+  bool _actionTimeStarted = false;
+
+  // List to store the action times for each item
+  List<String> _itemActionTimes = [];
+
+  // Variable to store the previous lapsed time
+  String _previousLapsedTime = '';
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +55,7 @@ class _RecordingPageState extends State<RecordingPage> {
                 color: Colors.white,
               ),
               child: IconButton(
-                onPressed: _handlePauseButtonPress, // Use the new method
+                onPressed: _handlePauseButtonPress,
                 icon: Icon(
                   _isPlaying ? Icons.stop : Icons.play_arrow,
                   color: Color(0xFF1C1D1C),
@@ -84,14 +94,16 @@ class _RecordingPageState extends State<RecordingPage> {
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
                       return _buildWhiteBox(
-                          "Current Time", _formatTime(snapshot.data!));
+                        "Current Time",
+                        _formatTime(snapshot.data!),
+                      );
                     } else {
                       return _buildWhiteBox("Current Time", "");
                     }
                   },
                 ),
-                _buildWhiteBox("Action Time", ""),
-                _buildWhiteBox("Lapsed Time", ""),
+                _buildWhiteBox("Action Time", _getActionTimeText()),
+                _buildWhiteBox("Lapsed Time", _previousLapsedTime), // Display previous lapsed time
               ],
             ),
           ),
@@ -105,7 +117,7 @@ class _RecordingPageState extends State<RecordingPage> {
               children: [
                 _buildWhiteBox("Last Action", _getLastActionText()),
                 _buildWhiteBox("Action Count", _actionCount.toString()),
-                _buildWhiteBox("Start Time", _startTime),
+                _buildWhiteBox("Start Time", _formatTime(_startTime)),
               ],
             ),
           ),
@@ -116,7 +128,7 @@ class _RecordingPageState extends State<RecordingPage> {
                 crossAxisCount: 3,
                 crossAxisSpacing: 2,
                 mainAxisSpacing: 2,
-                childAspectRatio: 135 / 70, // Set the aspect ratio of each item
+                childAspectRatio: 135 / 70,
               ),
               itemCount: widget.variableList.length,
               itemBuilder: (context, index) {
@@ -133,10 +145,15 @@ class _RecordingPageState extends State<RecordingPage> {
                       _selectedIndex = isSelected ? -1 : index;
                       if (isSelected) {
                         _actionCount++;
+                        if (_startTime == null || _prevSelectedIndex == -1) {
+                          _startTime = DateTime.now();
+                        }
+                        _startActionTime();
                       }
                       if (_prevSelectedIndex != -1) {
-                        // Store the last variable name for the previously selected item
-                        _lastActionMap[_selectedIndex] = widget.variableList[_prevSelectedIndex]['name']!;
+                        _lastActionMap[_selectedIndex] =
+                            widget.variableList[_prevSelectedIndex]['name']!;
+                        _storeItemActionTime();
                       }
                     });
                   },
@@ -145,9 +162,11 @@ class _RecordingPageState extends State<RecordingPage> {
                       color: isSelected
                           ? Colors.black
                           : (variableType == 'Waste'
-                          ? Colors.red
-                          : Colors.green),
-                      border: isSelected ? Border.all(color: Colors.red, width: 1) : null,
+                              ? Colors.red
+                              : Colors.green),
+                      border: isSelected
+                          ? Border.all(color: Colors.red, width: 1)
+                          : null,
                     ),
                     child: Align(
                       alignment: Alignment.center,
@@ -176,15 +195,34 @@ class _RecordingPageState extends State<RecordingPage> {
     setState(() {
       _isPlaying = !_isPlaying;
       if (_isPlaying) {
-        _startTime = _formatTime(DateTime.now());
+        if (_pauseTime != null) {
+          // Calculate the difference and add it to the start time
+          final pausedDuration = DateTime.now().difference(_pauseTime!);
+          if (_startTime != null) {
+            _startTime = _startTime!.add(pausedDuration);
+          }
+        } else {
+          _startTime = DateTime.now();
+        }
+
+        if (!_actionTimeStarted) {
+          _startActionTime();
+          _actionTimeStarted = true;
+        }
       } else {
-        // Reset the action count when the pause button is pressed
         _actionCount = 0;
+        _actionTimeStarted = false;
+
+        // Stop the action time timer when pausing
+        _actionTimeTimer?.cancel();
+
+        // Store pause time
+        _pauseTime = DateTime.now();
       }
     });
   }
 
-  Widget _buildWhiteBox(String title, String text) {
+  Widget _buildWhiteBox(String title, String? text) {
     return Container(
       width: 135,
       height: 70,
@@ -201,7 +239,7 @@ class _RecordingPageState extends State<RecordingPage> {
           ),
           SizedBox(height: 5),
           Text(
-            text,
+            text ?? '',
             style: TextStyle(color: Colors.green, fontWeight: FontWeight.normal),
           ),
         ],
@@ -209,12 +247,61 @@ class _RecordingPageState extends State<RecordingPage> {
     );
   }
 
-  String _formatTime(DateTime time) {
-    return DateFormat('HH:mm:ss').format(time);
+  String _formatTime(DateTime? time) {
+    if (time != null) {
+      return DateFormat('HH:mm:ss').format(time);
+    } else {
+      return '';
+    }
   }
 
   String _getLastActionText() {
-    // Return the last variable name for the selected item
-    return _lastActionMap[_selectedIndex] ?? ''; // Provide a default value for null case
+    return _lastActionMap[_selectedIndex] ?? '';
+  }
+
+  void _startActionTime() {
+    _actionTimeTimer = Timer.periodic(Duration(seconds: 1), (_) {
+      setState(() {
+        if (_startTime != null) {
+          final currentTime = DateTime.now();
+          final difference = currentTime.difference(_startTime!);
+          _previousLapsedTime = _formatDuration(difference);
+        }
+      });
+    });
+  }
+
+  String _getActionTimeText() {
+    if (_selectedIndex != -1 && _startTime != null) {
+      final currentTime = DateTime.now();
+      final difference = currentTime.difference(_startTime!);
+      return _formatDuration(difference);
+    } else {
+      return '';
+    }
+  }
+
+  void _storeItemActionTime() {
+    if (_selectedIndex != -1 && _startTime != null) {
+      final currentTime = DateTime.now();
+      final difference = currentTime.difference(_startTime!);
+      _itemActionTimes.add(_formatDuration(difference));
+    }
+  }
+
+  String _formatDuration(Duration duration) {
+    final hours = duration.inHours.toString().padLeft(2, '0');
+    final minutes = (duration.inMinutes % 60).toString().padLeft(2, '0');
+    final seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
+    return '$hours:$minutes:$seconds';
+  }
+
+  DateTime? _pauseTime; // Store the pause time
+
+  @override
+  void dispose() {
+    // Cancel the action time timer when disposing of the widget
+    _actionTimeTimer?.cancel();
+    super.dispose();
   }
 }
